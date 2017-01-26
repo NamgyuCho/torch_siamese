7c3dacc64df08750b84f1f4137df6f33db657de7
Namgyu Cho <NamgyuCho@users.noreply.github.com>
2017-01-26 00:35:21 -0800

    update

---
+++ model.lua
@@ -0,0 +1,29 @@
+require 'nn'
+
+function build_model(libs)
+    local SpatialConvolution = libs['SpatialConvolution']
+    local SpatialMaxPooling = libs['SpatialMaxPooling']
+    local ReLU = libs['ReLU']
+
+    --Encoder / Embedding
+    --Input dims are 28x28
+    encoder = nn.Sequential()
+    encoder:add(nn.SpatialConvolution(1, 20, 5, 5))
+    encoder:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+    encoder:add(nn.SpatialConvolution(20, 50, 5, 5))
+    encoder:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+
+    encoder:add(nn.View(50*5*5))
+    encoder:add(nn.Linear(50*5*5, 500))
+    encoder:add(nn.ReLU())
+    encoder:add(nn.Linear(500, 10))
+
+    -- The siamese model
+    siamese_encoder = nn.ParallelTable()
+    siamese_encoder:add(encoder)
+    siamese_encoder:add(encoder:clone('weight', 'bias', 'gradWeight', 'gradBias')) --clone the encoder and share the weight, bias. Must also share the gradWeight and gradBias
+
+    -- The siamese model (inputs will be Tensors of shape (2, channel, height, width))
+    model = nn.Sequential()
+    model:add(nn.SplitTable(1))
+end
